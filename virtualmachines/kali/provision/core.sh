05f52ede0c84d485277bf21b58293f20f7e13b43
Cristian  Chaparro <cristianchaparro@192.168.68.58>
2022-05-24 00:45:57 -0500

    add missing drivers

--- virtualmachines/kali/provision/core.sh
+++ virtualmachines/kali/provision/core.sh
@@ -8,3 +8,4 @@ sudo apt-get dist-upgrade
 sudo apt-get install aptitude -y
 sudo apt-get install build-essential bc libelf-dev linux-headers-`uname -r` -y
 
+sudo apt install net-tools wireless-tools -y 
