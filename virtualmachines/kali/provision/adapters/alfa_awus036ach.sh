695b7f283d93528ea81758ff8f3bdf2d6c339e71
Cristian <cristianchaparroa@users.noreply.github.com>
2022-05-24 00:51:09 -0500

    Vagrant/kali optimization (#4)
    * feat: use the custom vagrant box
    
    * add missing drivers
    
    Co-authored-by: Cristian  Chaparro <cristianchaparro@192.168.68.58>

--- virtualmachines/kali/provision/adapters/alfa_awus036ach.sh
+++ virtualmachines/kali/provision/adapters/alfa_awus036ach.sh
@@ -3,7 +3,7 @@
 sudo apt remove realtek-rtl88xxau-dkms 
 sudo apt purge realtek-rtl88xxau-dkms
 sudo apt autoremove && apt autoclean
-
+sudo apt install realtek-rtl88xxau-dkms -y 
 
 mkdir drivers &&cd drivers
 git clone https://github.com/aircrack-ng/rtl8812au
--- virtualmachines/kali/provision/core.sh
+++ virtualmachines/kali/provision/core.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee -a /etc/apt/sources.list
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade 
 
-sudo apt install aptitude -y
-sudo apt install build-essential bc libelf-dev linux-headers-`uname -r` -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade 
 
+sudo apt-get install aptitude -y
+sudo apt-get install build-essential bc libelf-dev linux-headers-`uname -r` -y
+
+sudo apt install net-tools wireless-tools -y 
