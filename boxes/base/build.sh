68d376c7c21584e00e6bba1ce840abe7f598b6f2
cristianchaparroa <cristianchaparroa@gmail.com>
2022-08-23 20:50:04 -0500

    start to configuring the unatended process

--- boxes/base/build.sh
+++ boxes/base/build.sh
@@ -14,14 +14,7 @@ hddname=${disks}/${vmname}.vdi
 
 # create the virtual machine
 echo "---> creating the virtual machine ..."
-VBoxManage createvm --name ${vmname} --ostype Linux64  --register
-
-
-# set the memory and network
-echo "---> configuring the virtual machine ..."
-VBoxManage modifyvm ${vmname} --ioapic on
-VBoxManage modifyvm ${vmname} --memory 1024 --vram 128
-VBoxManage modifyvm ${vmname} --nic1 nat
+VBoxManage createvm --name ${vmname} --ostype Debian_64  --register
 
 
 # create the disk 
@@ -38,3 +31,17 @@ VBoxManage storageattach ${vmname} --storagectl ${satactl} --port 0 --device 0 -
 echo "---> adding the iso image"
 VBoxManage storagectl ${vmname} --name ${idectl} --add ide
 VBoxManage storageattach ${vmname} --storagectl ${idectl} --port 0 --device 0 --type dvddrive --medium ${iso}
+
+# set the memory and network
+echo "---> configuring the virtual machine ..."
+VBoxManage modifyvm ${vmname} --ioapic on
+VBoxManage modifyvm ${vmname} --memory 1024 --vram 128
+VBoxManage modifyvm ${vmname} --nic1 nat
+VBoxManage modifyvm ${vmname} --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+echo "---> starting installation ..."
+VBoxManage unattended install ${vmname} --iso=${iso} \
+    --user=root \
+    --password=vagrant 
+    #--script-template=minimal-preseed.cfg #\
+    # --type headless uncomment this line after everything has been tested
