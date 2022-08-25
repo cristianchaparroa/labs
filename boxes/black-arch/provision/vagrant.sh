650ec999a4d758ac093304462ec032e72cf32c39
Cristian <cristianchaparroa@users.noreply.github.com>
2022-08-25 01:52:36 -0500

    configure the blackarch base box and the vm (#13)

---
+++ boxes/black-arch/provision/vagrant.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+# Add insecure public key.
+rm -Rf /home/vagrant/.ssh
+mkdir /home/vagrant/.ssh
+wget -O /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+
+# enable the ssh service
+sudo systemctl enable sshd.service
+sudo systemctl start sshd.service
