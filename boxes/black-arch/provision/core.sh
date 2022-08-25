23511192d356e8c04f1b47357ec1b1db2080ea2f
cristianchaparroa <cristianchaparroa@gmail.com>
2022-08-25 01:51:41 -0500

    configure the blackarch base box and the vm

--- boxes/black-arch/provision/core.sh
+++ boxes/black-arch/provision/core.sh
@@ -1,12 +1,15 @@
 #!/bin/bash 
 
 # Run https://blackarch.org/strap.sh as root and follow the instructions.
-$ curl -O https://blackarch.org/strap.sh
+curl -O https://blackarch.org/strap.sh
 # Verify the SHA1 sum
-$ echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
+echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
 # Set execute bit
-$ chmod +x strap.sh
+chmod +x strap.sh
 # Run strap.sh
-$ sudo ./strap.sh
+sudo ./strap.sh
 # Enable multilib following https://wiki.archlinux.org/index.php/Official_repositories#Enabling_multilib and run:
-$ sudo pacman -Syu
+sudo pacman -Syu
+
+sudo pacman -Sy wget --noconfirm
+
